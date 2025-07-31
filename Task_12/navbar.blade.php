7f3362833bcc7420c748a4b39a1ef41a1669a561
Rachel8712 <adellhusna@gmail.com>
2025-07-31 16:26:50 +0700

    navbar_blade.php

---
+++ Task_12/navbar.blade.php
@@ -0,0 +1,16 @@
+<!-- Header -->
+<header class="bg-[#CAA494] text-white" id="header-section">
+        <div class="container mx-auto px-4 py-6">
+            <div class="flex justify-between items-center">
+                <div class="text-2xl font-bold">AdelysiaCoffee.com</div>
+                <nav>
+                    <ul class="flex space-x-6">
+                        <li><a href="#hero-section" class="hover:text-[#63612C]">Beranda</a></li>
+                        <li><a href="#products-section" class="hover:text-[#63612C]">Produk</a></li>
+                        <li><a href="#" class="hover:text-[#63612C]">Tentang Kami</a></li>
+                        <li><a href="#" class="hover:text-[#63612C]">Kontak</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
