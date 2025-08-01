6f486fa30368caccf9e5123b1821c25239741b6d
Rachel8712 <adellhusna@gmail.com>
2025-08-01 12:14:47 +0700

    Product_Category_Seeder.php

---
+++ Task_13/Product_Category_Seeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('product_categories')->insert([
+            ['name' => 'Classic Coffee'],
+            ['name'=> 'Classic Tea'],
+            ['name'=> 'Non-Coffee'],
+        ]);
+    }
+}
