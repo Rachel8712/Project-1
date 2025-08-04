bcf001cc74960c8f4f896480490852732ebfd16b
Rachel8712 <adellhusna@gmail.com>
2025-08-04 18:02:35 +0700

    web._laravel_breeze_up.php

---
+++ Task_16/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+
+Route::get("/", [HomeController::class,"index"]);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/products', function () {
+  return view('dashboard.products.index');
+})-> name('products');
+
+Route::get('/category-products', function () {
+  return view('dashboard.category_products.index');
+})-> name('category-products');
