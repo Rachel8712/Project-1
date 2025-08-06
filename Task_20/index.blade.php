77a7a0c3faf62a07881916c21f8d2f140fb12efe
Rachel8712 <adellhusna@gmail.com>
2025-08-06 20:31:46 +0700

    products_index_20

--- Task_20/index.blade.php
+++ Task_20/index.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-white leading-tight">
-            {{ __('Daftar Kategori Produk') }}
+            {{ __('Daftar Produk') }}
         </h2>
     </x-slot>
     <div class="container mt-5">
         <div class="header-section">
             <div>
-                <a href="{{ route('category-products.create') }}" class="btn btn-primary">
-                    <i class="fas fa-plus"></i> Tambah Kategori
+                <a href="{{ route('products.create') }}" class="btn btn-primary">
+                    <i class="fas fa-plus"></i> Tambah Produk
                 </a>
             </div>
         </div>
@@ -17,11 +17,11 @@
             <div class="card-header">
                 <div class="row">
                     <div class="col-md-6">
-                        <h5>Daftar Kategori</h5>
+                        <h5>Daftar Produk</h5>
                     </div>
                     <div class="col-md-6">
                         <div class="input-group mb-3 search-box">
-                            <input type="text" class="form-control" placeholder="Cari kategori...">
+                            <input type="text" class="form-control" placeholder="Cari produk...">
                             <button class="btn btn-outline-secondary" type="button">
                                 <i class="fas fa-search"></i>
                             </button>
@@ -35,19 +35,25 @@
                         <thead class="table-dark">
                             <tr>
                                 <th>ID</th>
-                                <th>Nama Kategori</th>
-                                <th>Jumlah Produk</th>
+                                <th>Nama Produk</th>
+                                <th>Deskripsi</th>
+                                <th>Stok</th>
+                                <th>Harga</th>
+                                <th>Gambar</th>
                                 <th>Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($categories as $category)
+                            @foreach ($products as $product )
                             <tr>
-                                <td>{{$category->id}}</td>
-                                <td>{{$category->name}}</td>
-                                <td>{{$category->product_count}}</td>
+                                <td>{{ $product->id }}</td>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->description }}</td>
+                                <td>{{$product->stock}}</td>
+                                <td>{{$product->price}}</td>
+                                <td><img src= "{{ asset('storage/'. $product->image) }}" alt={{ $product->name }} class="img-fluid" style="max-height: 150px"></td>
                                 <td>
-                                    <a href="{{ route('category-products.edit', $category->id) }}" class="btn btn-sm btn-warning action-btn">
+                                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-warning action-btn">
                                         <i class="fas fa-edit"></i> Edit
                                     </a>
                                     <button class="btn btn-sm btn-danger action-btn" data-bs-toggle="modal" data-bs-target="#hapusModal">
