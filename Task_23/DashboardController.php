5b2724128c88c5da98af307f826488a18e97b3b6
Rachel8712 <adellhusna@gmail.com>
2025-08-11 13:46:05 +0700

    dashboard_controller_23

---
+++ Task_23/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        //Menampilkan jumlah produk
+        $jumlahProduk = Product::count();
+
+        //Menghitung total klik
+        $jumlahKlikProduk = Product::sum('click');
+
+        //Menampilkan jumlah kategori produk
+        $jumlahKategoriProduk = ProductCategory::count();
+
+        return view("dashboard",
+        compact(
+            "jumlahProduk",
+            "jumlahKlikProduk",
+            "jumlahKategoriProduk"
+            ));
+    }
+}
